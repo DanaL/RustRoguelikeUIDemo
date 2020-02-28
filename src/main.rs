0edda449d0c03dd792b5b7a8980e3f2e9c5044e7
Dana Larose <ywg.dana@gmail.com>
2020-02-28 09:00:33 -0600

    Cave generator done

--- src/main.rs
+++ src/main.rs
@@ -573,10 +573,10 @@ fn run(map: &Vec<Vec<map::Tile>>) -> Result<(), String> {
 fn main() -> Result<(), String> {
 	let map = map::generate_island(65);
 	//let map = map::generate_test_map();
-	map::generate_cave(15, 15);
+	let map = map::generate_cave(20, 15);
 	//let path = pathfinding::find_path(&map, 4, 4, 9, 9);
 	//println!("{:?}", path);
-	//run(&map)?;
+	run(&map)?;
 
     Ok(())
 }
